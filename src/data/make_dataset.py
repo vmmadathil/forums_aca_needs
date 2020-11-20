51502bcd95b8704e348e52cb1cb52f0e9bafb002
vmmadathil <madathilvisakh@gmail.com>
2020-11-20 03:22:28 -0600

    'idk'

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,32 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from datetime import datetime
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+
+def cleancsv(input_filepath):
+    df = pd.read_csv(input_filepath)
+
+    
+
+    #df['time_created'] = datetime.fromtimestamp(df['created_utc'].astype(float))
+    df = df.drop(columns = ['url'])
+
+    print(df.head())
+
+
+
+
+
+""" 
+Runs data processing scripts to turn raw data from (../raw) into
+cleaned data ready to be analyzed (saved in ../processed).
+"""
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
+    
+    cleancsv(input_filepath)
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    input_file = '../../data/raw/raw_reddit_scrap.csv'
+    output_file = '../../data/process/cleaned_reddit_data.csv'
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(input_file, output_file)
