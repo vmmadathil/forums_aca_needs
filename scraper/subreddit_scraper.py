ec1ee99a02005632f0094d6bca500f7e250f3c36
vmmadathil <madathilvisakh@gmail.com>
2020-11-20 01:50:14 -0600

    finished scraping raw reddit data

--- scraper/subreddit_scraper.py
+++ scraper/subreddit_scraper.py
@@ -3,36 +3,24 @@ This script will scrape the health insurance subreddit for posts relating to the
 '''
 from psaw import PushshiftAPI
 import pandas as pd
-import datetime
+import datetime as dt
 
 api = PushshiftAPI()
 
-#starting and ending epoch dates
-curr_epoch = 1605772907
-end_epoch = 1605757257
+#calling the function to fetch all the posts
+gen = api.search_submissions(subreddit= 'HealthInsurance',
+                             after= 1356998400,
+                             filter = ['url', 'title', 'selftext', 'created_utc'])
 
-print('beginning scraping')
 
-#getting the comments with 1000 post batches
-while (curr_epoch >= end_epoch):
-    gen = api.search_submissions(subreddit='HealthInsurance',
-                                after=curr_epoch,
-                                filter = ['url', 'title', 'selftext', 'created_utc'],
-                                limit = 1000)
-    #converting to dataframe and appending to the list
-    df  = pd.DataFrame([thing.d_ for thing in gen])
-    li.append(df)
+#converting to dataframe and appending to the list
+df  = pd.DataFrame([thing.d_ for thing in gen])
 
-    #setting current date as latest processed date
-    curr_epoch = df['created_utc'].tail(1) 
-    
-    #printing progress message
-    curr_time = datetime.datetime.fromtimestamp(curr_time)
-    print("Currently scraped until {}").format(curr_time)
+print(df.info())
 
-    filename = ('raw_scrape_{}.csv').format(curr_time)
+print('finished scraping, saving to csv')
 
-    #saving to csv
-    df.to_csv(filename)
+#print to a csv
+df.to_csv('../data/raw/raw_reddit_scrap.csv')
 
-print('finished scraping')
+print('saved to csv')
