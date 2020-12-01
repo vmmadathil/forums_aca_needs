a78a8e0e08b70ba07a278859eac2a80525ac90df
vmmadathil <madathilvisakh@gmail.com>
2020-12-01 02:27:41 -0600

    finished text cleaning

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,7 +4,7 @@ import re
 
 
 def cleancsv(input_filepath, output_filepath):
-    df = pd.read_csv(input_filepath)
+    df = pd.read_csv(input_filepath, encoding='utf-8')
 
     #filtering out entries that have no text
     df = df[df['selftext'].notnull()]
@@ -13,14 +13,24 @@ def cleancsv(input_filepath, output_filepath):
     df['time_created'] = pd.to_datetime(df['created_utc'], unit='s')
     df = df.drop(columns = ['url', 'created_utc', 'created'])
 
+    #removing rows that are deleted, removed, and blank comments
+    df = df.loc[df['selftext'] != '[deleted]']
+    df = df.loc[df['selftext'] != '[removed]']
+    df = df.loc[df['selftext'] != '']
+
     #processing text
     #removing strings that have urls
     df['processed_text'] = df['selftext'].map(lambda x: re.sub(r'(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?',"",x,flags=re.MULTILINE))
     #df['processed_text'] = df['selftext'].map(lambda x: re.sub(r'^www\/\/.*[\r\n]*','', x, flags=re.MULTILINE))
 
+    #removing blank comments
+    df = df.loc[df['processed_text'] != '']
 
     #removing puncutation
     df['processed_text'] = df['processed_text'].map(lambda x: re.sub('[,\.!?]', '', x))
+
+    #fixing apostrophes
+    #df['processed_text'] = df['processed_text'].replace({"â€™" : "'"}, regex=True)
 
     #lowercasing all the words
     df['processed_text'] = df['processed_text'].map(lambda x: x.lower())
@@ -29,7 +39,7 @@ def cleancsv(input_filepath, output_filepath):
     print(df.head())
 
     #printing to csv
-    df.to_csv(output_filepath, index=False)
+    df.to_csv(output_filepath, index=False, encoding='utf-8-sig')
 
 
 """ 
